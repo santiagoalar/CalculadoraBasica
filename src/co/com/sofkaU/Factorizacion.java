b974b09a3c400941fe4b93e5888be5cfdc7fad1b
Santiago Alarcon <santiago-alar@hotmail.com>
2021-05-19 12:24:47 -0500

    Create factorizar method and clean Main.java

--- src/co/com/sofkaU/Factorizacion.java
+++ src/co/com/sofkaU/Factorizacion.java
@@ -1,7 +1,21 @@
 package co.com.sofkaU;
 
+import java.util.Scanner;
+
 public class Factorizacion {
-    public static void main(String[] args){
-        System.out.println("Bienvenidos al menu");
+
+    public double factorizar(int number){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingrese le numero factorial:  ");
+        number = in.nextInt();
+        double factorial = 1;
+        while (number != 0){
+            factorial=factorial*number;
+            number--;
+        }
+        //System.out.println(factorial);
+        return factorial;
     }
+
+
 }
--- src/co/com/sofkaU/Main.java
+++ src/co/com/sofkaU/Main.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Ingrese le numero factorial:  ");
-        int number = in.nextInt();
-        double factorial = 1;
-        while (number != 0){
-            factorial=factorial*number;
-            number--;
-        }
-        System.out.println(factorial);
+        System.out.println("Bienvenidos al menu");
     }
 }
