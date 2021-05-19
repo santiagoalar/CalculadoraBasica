0abb51ff6727878327f7898ddfdbcc5c12162346
Santiago Alarcon <santiago-alar@hotmail.com>
2021-05-19 12:07:50 -0500

    Solucion final factorial.java

--- src/co/com/sofkaU/Main.java
+++ src/co/com/sofkaU/Main.java
@@ -1,8 +1,18 @@
 package co.com.sofkaU;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-    System.out.println("Bienvenidos al menu");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ingrese le numero factorial");
+        int number = in.nextInt();
+        double factorial = 1;
+        while (number != 0){
+            factorial=factorial*number;
+            number--;
+        }
+        System.out.println(factorial);
     }
 }
