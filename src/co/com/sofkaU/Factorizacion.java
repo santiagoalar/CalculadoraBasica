3d8d3a4d68dcf33bfb25c004da481d6f14ea2cb6
Santiago Alarcon <santiago-alar@hotmail.com>
2021-05-19 13:40:15 -0500

    Se elimina la opcion la propiedad del metodo factorizar en Factorizacion.java

--- src/co/com/sofkaU/Factorizacion.java
+++ src/co/com/sofkaU/Factorizacion.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Factorizacion {
 
-    public static double factorizar(int number){
+    public static double factorizar(){
         Scanner in = new Scanner(System.in);
         System.out.println("Ingrese le numero factorial:  ");
-        number = in.nextInt();
+        int number = in.nextInt();
         double factorial = 1;
         while (number != 0){
             factorial=factorial*number;
--- src/co/com/sofkaU/Main.java
+++ src/co/com/sofkaU/Main.java
@@ -1,8 +1,13 @@
 package co.com.sofkaU;
 
+import static co.com.sofkaU.Factorizacion.factorizar;
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Bienvenidos al menu");
+
+        System.out.println("El factorial es: " + factorizar());
+
     }
 }
